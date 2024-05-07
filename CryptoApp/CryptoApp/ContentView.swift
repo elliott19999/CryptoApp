2fb382a77bf4136385336cf3f6c5cce128a48878
elliott <elliott>
2024-05-08 00:35:46 +0300

    no message

--- CryptoApp/CryptoApp/ContentView.swift
+++ CryptoApp/CryptoApp/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.backround
+                .ignoresSafeArea()
+            VStack {
+                Text("Accent Color")
+                    .foregroundStyle(Color.theme.accent)
+            }
+            .font(.headline)
         }
-        .padding()
     }
 }
 
--- CryptoApp/CryptoApp/Extensions/Color.swift
+++ CryptoApp/CryptoApp/Extensions/Color.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let backround = Color("BackroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
